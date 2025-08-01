24c14be468f44910e982589a2a1da51a40ab2e42
maycon-douglas2005 <mayconchaves2005@gmail.com>
2025-08-01 18:51:46 -0300

    Melhorando robustez do controle de erros aplicando Double.TryParse() em todos os procedimentos.

--- revisao/Module1.vb
+++ revisao/Module1.vb
@@ -4,10 +4,11 @@
     Dim saldo As Double
 
     Sub Main()
-        Dim informacoesCompletas As Boolean
+        Dim informacoesCompletas As Boolean = False
+        Dim sucessoConversaoSaldo = False
         Dim respostaInformacoes As String
         Dim acao As String
-        informacoesCompletas = False
+
         saldo = 0.0
 
 
@@ -18,8 +19,18 @@
             Console.WriteLine("Ótimo! {0}, digite sua idade: ", nome)
             idade = Console.ReadLine()
 
-            Console.WriteLine("Perfeito! Agora só falta digitar seu salário: ")
-            saldo = CDbl(Console.ReadLine())
+
+            Do Until sucessoConversaoSaldo
+                Console.WriteLine("Perfeito! Agora só falta digitar seu salário: ")
+                If (Double.TryParse(Console.ReadLine(), saldo)) Then
+                    Console.WriteLine("Salário armazenado com sucesso!")
+                    sucessoConversaoSaldo = True
+                Else
+                    Console.WriteLine("Ops, parece que você digitou um valor inválido, por favor digite seu salário no padrao xxxx.xx")
+                End If
+
+            Loop
+
 
             Console.WriteLine("Apenas para revisar, confirme com s para sim ou n para nao se os seus dados estão corretos:
             Nome: {0}, Idade: {1}, Salário: {2}", nome, idade, saldo)
@@ -45,7 +56,7 @@
                 Console.WriteLine("Seu saldo é {0}", verificacaoDeSaldo)
                 Console.WriteLine("Verificação de saldo concluida!")
 
-            ElseIf acao = "sair" Then
+            ElseIf acao = "sair" OrElse "deslogar" Then
                 Console.WriteLine("Finalizando programa...")
                 Environment.Exit(0)
 
@@ -58,12 +69,24 @@
     End Sub
 
     Sub depositar()
+        Dim depositoConvertido As Boolean = False
         Dim valorDeposito As Double
-        Console.WriteLine("{0}, digite o valor que deseja depositar: ", nome)
-        valorDeposito = CDbl(Console.ReadLine())
 
-        saldo += valorDeposito
-        Console.WriteLine("Depósito realizado com sucesso!")
+        Do Until depositoConvertido
+            Console.WriteLine("{0}, digite o valor que deseja depositar: ", nome)
+            If Double.TryParse(Console.ReadLine(), valorDeposito) Then
+                Console.WriteLine("Depósito recebido com sucesso!")
+                saldo += valorDeposito
+                depositoConvertido = True
+            Else
+                Console.WriteLine("Ops, parece que você digitou um valor errado! Por favor, digite um valor valido no padrão xxxx.xx")
+            End If
+        Loop
+
+
+
+
+
     End Sub
 
     Function verificarSaldo() As Double
@@ -71,12 +94,25 @@
     End Function
 
     Sub sacar()
+        Dim conversaoSaque As Boolean = False
         Dim valorSaque As Double
-        Console.WriteLine("{0}, digite o valor que deseja sacar: ", nome)
-        valorSaque = CDbl(Console.ReadLine())
 
-        saldo -= valorSaque
-        Console.WriteLine("O saque de {0} foi realizado com sucesso!", valorSaque)
+        Do Until conversaoSaque
+            Console.WriteLine("Digite o valor que deseja sacar: ")
+            If Double.TryParse(Console.ReadLine(), valorSaque) Then
+                If saldo > valorSaque Then
+                    saldo -= valorSaque
+                    Console.WriteLine("Saque realizado com sucesso!")
+                    conversaoSaque = True
+                Else
+                    Console.WriteLine("Saldo insuficiente! O valor que deseja sacar é maior do que você tem de saldo.")
+                End If
 
+            Else
+                Console.WriteLine("Ops, parece que você digitou um valor inválido! Por favor, digite um valor no padrão xxxx.xx")
+            End If
+        Loop
     End Sub
+
+
 End Module
